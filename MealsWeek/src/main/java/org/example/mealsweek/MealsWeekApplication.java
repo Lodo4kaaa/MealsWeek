d1d3367af4d71aee9acce2009f509706deca1407
Natrodip <vank.gulidov@mail.ru>
2026-03-01 19:39:46 +0300

    init-commit

---
+++ MealsWeek/src/main/java/org/example/mealsweek/MealsWeekApplication.java
@@ -0,0 +1,13 @@
+package org.example.mealsweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsWeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealsWeekApplication.class, args);
+    }
+
+}
---
+++ MealsWeek/src/test/java/org/example/mealsweek/MealsWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mealsweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsWeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
